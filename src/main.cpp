9a7ffad5908906e9f2917755a46b4fe4ac43b115
Aslati <joajabcz@gmail.com>
2024-05-24 20:05:15 +0200

    Plot

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,35 @@
 #include <pybind11/pybind11.h>
+#include <matplot/matplot.h>
+#include <vector>
+#include <cmath>
+#include <math.h>
+// #include <AudioFile.h>
+#include <corecrt_math_defines.h>
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
 
-int add(int i, int j) {
-    return i + j;
-}
+namespace mp = matplot;
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(_core, m) {
+int add(int i, int j)
+{
+    return i + j;
+}
+
+void plot()
+{
+    std::vector<double> x = mp::linspace(0, 2 * M_PI);
+    std::vector<double> y = mp::transform(x, [](auto x)
+                                          { return sin(x); });
+    mp::plot(x, y);
+
+    mp::show();
+}
+
+PYBIND11_MODULE(_PROJ3, m)
+{
     m.doc() = R"pbdoc(
         Pybind11 example plugin
         -----------------------
@@ -29,15 +49,14 @@ PYBIND11_MODULE(_core, m) {
         Some other explanation about the add function.
     )pbdoc");
 
-    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
+    m.def("subtract", [](int i, int j)
+          { return i - j; }, R"pbdoc(
         Subtract two numbers
 
         Some other explanation about the subtract function.
     )pbdoc");
 
-#ifdef VERSION_INFO
-    m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
-#else
-    m.attr("__version__") = "dev";
-#endif
+    m.def("plot", &plot, R"pbdoc(a plotting function)pbdoc");
+
+    m.attr("__version__") = "cryingOutLoud";
 }
